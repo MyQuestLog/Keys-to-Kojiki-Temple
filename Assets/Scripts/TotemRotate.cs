f5435e08475f4a8f0355cb629cb180c10529c7ee
DeclanTanner <116156795+Winnowmaker@users.noreply.github.com>
2023-04-28 11:15:51 +1000

    Started Totem Puzzle and Interactor Script

---
+++ Assets/Scripts/TotemRotate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotemRotate : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+
+    private GameObject Totem1;
+
+    void Awake()
+    {
+       
+    }
+}
