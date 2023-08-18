803ea362b1a58456c5f59cb11f1c88b5ff8cc47f
Owen <105467906+OwenPallister@users.noreply.github.com>
2023-08-18 14:27:35 +1000

    Added new scenes
    Added main menu, added options menu

--- Assets/Scripts/TotemPuzzle/DoorMovement.cs
+++ Assets/Scripts/TotemPuzzle/DoorMovement.cs
@@ -1,22 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 using static DoorButton;
 public class DoorMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void PlayGame ()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_puzzleCorrect == true)
-        {
-            transform.position += new Vector3(0, -1 * Time.deltaTime, 0);
-        }
-    }
 }
